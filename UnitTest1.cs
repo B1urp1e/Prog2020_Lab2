560538e81f84e9b5d1806b5cd63b68c85090280b
B1urp1e <70600555+B1urp1e@users.noreply.github.com>
2021-09-18 14:59:20 -0400

    Add files via upload
    Unit Test MissingAngle(); test added and failed as logic not created

---
+++ UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Triangle_lab2;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestPythagorus()
+        {
+            PythagTest obj = new PythagTest();
+            double result = obj.MissingSide(5, 10);
+            Assert.AreEqual(result, Math.Sqrt(125));
+        }
+        [TestMethod]
+        public void TestAngles()
+        {
+            PythagTest obj = new PythagTest();
+            int result = obj.MissingAngle(34, 62);
+            Assert.AreEqual(result, 84);
+        }
+
+        [TestMethod]
+        public void TestArea()
+        {
+            PythagTest obj = new PythagTest();
+            double result = obj.Area(12, 9);
+            Assert.AreEqual(result, (0.5 * (12 * 9)));
+        }
+
+    }
+}
